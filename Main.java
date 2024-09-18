5a4f57056195c87d3678d533f26630fb8415782a
Bangar Mahammed Ghouse <111179871+MDGHOUSE18@users.noreply.github.com>
2024-09-18 10:35:02 +0530

    add main class

---
+++ Main.java
@@ -0,0 +1,49 @@
+package Assignment;
+
+
+class EmployeeWageComputation {
+    int wagePerHour;
+    int fullDay;
+    int halfDay;
+    int dailyWage;
+    int monthlyWage;
+    float counter1;
+    int totalHourPresent;
+    int totalHourPartTime;
+    int totalHourAbsent;
+    int empCheck;
+    int totalHours;
+    int totalNoOfDays;
+    String companyName;
+
+    EmployeeWageComputation(String companyName, int wage, int totalNoOfDays) {
+        this.wagePerHour = wage;
+        this.fullDay = 8;
+        this.halfDay = 4;
+        this.dailyWage = 0;
+        this.monthlyWage = 0;
+        this.counter1 = 0.0f;
+        this.totalHourPresent = 0;
+        this.totalHourPartTime = 0;
+        this.totalHourAbsent = 0;
+        this.totalHours = 0;
+        this.companyName = companyName;
+        this.totalNoOfDays = totalNoOfDays;
+    }
+
+    @Override
+    public String toString() {
+        return "EmployeeWageComputation [wagePerHour=" + wagePerHour + ", fullDay=" + fullDay + ", halfDay=" + halfDay
+                + ", dailyWage=" + dailyWage + ", monthlyWage=" + monthlyWage + ", counter1=" + counter1
+                + ", totalHourPresent=" + totalHourPresent + ", totalHourPartTime=" + totalHourPartTime
+                + ", totalHourAbsent=" + totalHourAbsent + ", empCheck=" + empCheck + ", totalHours=" + totalHours
+                + ", totalNoOfDays=" + totalNoOfDays + ", companyName=" + companyName + "]";
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        EmployeeWageComputation tataMotors = new EmployeeWageComputation("Tata Motors", 20, 5);
+        System.out.println(tataMotors.toString());
+    }
+}
