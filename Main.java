7b49bf2f8eac7a4d03bc044ab681e8e8ebfc78c7
Bangar Mahammed Ghouse <111179871+MDGHOUSE18@users.noreply.github.com>
2024-09-18 10:48:33 +0530

    update attendance function with details

--- Main.java
+++ Main.java
@@ -46,16 +46,26 @@ public class Main {
         EmployeeWageComputation tataMotors = new EmployeeWageComputation("Tata Motors", 20, 5);
         System.out.println(tataMotors.toString());
 
+        int wagePerHour = 20;
+        int fullDay = 8;
         System.out.println("===== WELCOME TO EMPLOYEE WAGE COMPUTATION ======");
+        System.out.println("");
 
         int empCheck = (int) Math.floor(Math.random() * 10) % 2;
         if(empCheck == 0)
         {
-            System.out.println("Employee is Present");
+            System.out.print("Employee is Present");
+            System.out.println("");
+            System.out.print("The daily wage of Employee is : " + ( wagePerHour * fullDay ));
+            System.out.println("");
         }
         else
         {
-            System.out.println("Employee is Absent");
+            System.out.print("Employee is Absent");
+            System.out.println("");
+            System.out.println("The daily wage of Employee is : 0 " );
+            System.out.println("");
         }
+
     }
 }
